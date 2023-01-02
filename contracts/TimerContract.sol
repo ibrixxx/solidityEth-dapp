67ee2a6896d51b720ece7051379e3cef0650a3e1
ibrahimmesan <ibrahinmesan@gmail.com>
2023-01-02 21:27:53 +0100

    timer

--- contracts/TimerContract.sol
+++ contracts/TimerContract.sol
@@ -1,16 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
 import "hardhat/console.sol";
 import "./WavePortal.sol";
+import "@openzeppelin/contracts/ethereum-alarm-clock/EthereumAlarmClock.sol";
+
 
 contract TimerContract {
     WavePortal wavePortal;
+    EthereumAlarmClock eac = EthereumAlarmClock('');
+
 
     constructor(address _wavePortal) public {
         wavePortal = WavePortal(_wavePortal);
+        eac.scheduleCall(address(this), now + 1 days, 0, wave().encodeABI());
     }
 
     function wave() public {
         wavePortal.wave(1);
     }
 }
+
